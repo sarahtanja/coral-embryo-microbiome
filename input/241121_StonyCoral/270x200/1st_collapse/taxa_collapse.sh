6e2e1d2d48b85dcb310c316e0d4247e63945e0f2
sarahtanja <stanja@uw.edu>
2025-05-12 13:35:15 -0700

    removed duplicate files

--- input/241121_StonyCoral/270x200/1st_collapse/taxa_collapse.sh
+++
@@ -1,9 +0,0 @@
-# collapse at levels 1 (kingdom) through 7 (species)
-for LEVEL in {1..7}; do
-  qiime taxa collapse \
-    --i-table 250414_StonyCoral_270x200_featureTable.qza \
-    --i-taxonomy 250414_270x200_representative-sequences_taxonomy.qza \
-    --p-level $LEVEL \
-    --o-collapsed-table collapsed-l${LEVEL}.qza
-done
-
--- q2-picrust2-2023.2_0/q2_picrust2/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._full_pipeline import full_pipeline
-from ._custom_tree_pipeline import custom_tree_pipeline
-
-__all__ = ['custom_tree_pipeline', 'full_pipeline']
